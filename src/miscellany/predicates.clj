65eea22e20879ff073efd49cb6f0e3b891bb383b
Russell <russell@russelldunphy.com>
2014-09-06 12:35:42 +0100

    :books: Better docstrings

--- src/miscellany/predicates.clj
+++ src/miscellany/predicates.clj
@@ -4,23 +4,21 @@
   "Returns an anonymous function that takes one argument
   and passes it as the first argument to the supplied
   predicate function, along with any other arguments specified.
+
   Typical usage:
-  ```
   (filter (is > 3) [1 2 3 4 5 6])
-  ;; => [4 5 6]
-  ```"
+  ;; => [4 5 6]"
   [pred & args]
   (fn [x] (apply pred x args)))
 
 (defn has
   "Returns an anonymous function that takes one argument, calls
-  the supplied function on it, and passed the result as the first
-  argument to the supplied predicate function along with any
+  the supplied function on it, and passes the result as the first
+  argument to the supplied predicate function, along with any
   other arguments specified.
+
   Typical usage:
-  ```
   (filter (has count > 3) [\"cat\" \"dog\" \"fish\" \"tarantula\"])
-  ;; => [\"fish\" \"tarantula\"]
-  ```"
+  ;; => [\"fish\" \"tarantula\"]"
   [f pred & args]
   (fn [x] (apply pred (f x) args)))
